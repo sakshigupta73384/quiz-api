837de851ab88dc02df86983fcd23f0acff5f0d38
SAKSHI GUPTA <sakshig.fzdup@gmail.com>
2026-05-22 13:45:59 +0530

    Update QuizApplication.java

--- java/com/quiz/api/QuizApplication.java
+++ java/com/quiz/api/QuizApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuizApiApplication {
+public class QuizApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(QuizApiApplication.class, args);
+        SpringApplication.run(QuizApplication.class, args);
     }
 }
